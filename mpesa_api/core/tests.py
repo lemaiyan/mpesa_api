726b78aabea053806e32824d1a9a9b8f74535fc9
lemaiyan <george.kamunya@gmail.com>
2017-11-12 04:59:17 +0300

    Work done on tests so far

--- mpesa_api/core/tests.py
+++ mpesa_api/core/tests.py
@@ -1,3 +1,60 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+import requests
+from unittest import mock
+from django.conf import settings
+from mpesa_api.util.mpesautils import get_token
+from mpesa_api.core.models import B2CRequest, AuthToken
+from mpesa_api.core import tasks
+from mpesa_api.util import mocks
+from mpesa_api.util.b2cutils import send_b2c_request
 
-# Create your tests here.
+
+class MockResponse:
+    def __init__(self, json_data, status_code):
+        self.json_data = json_data
+        self.status_code = status_code
+
+    def json(self):
+        return self.json_data
+
+
+def mock_requests_get(*args, **kwargs):
+    url = kwargs['url']
+    if url == settings.GENERATE_TOKEN_URL:
+        return MockResponse(mocks.SUCCESS_TOKEN_REQUEST, 200)
+
+
+def mock_requests_post(*args, **kwargs):
+    url = kwargs['url']
+    if url == settings.B2C_URL:
+        return MockResponse(mocks.SUCCESS_B2C_SEND_RESPONSE, 200)
+
+
+class FetchTokenTest(TestCase):
+    @mock.patch('requests.get', side_effect=mock_requests_get)
+    def test_token_is_fetched_as_expected(self, mock_get):
+        response = get_token()
+        self.assertEqual(response, mocks.SUCCESS_TOKEN_REQUEST)
+        self.assertTrue(mock_get.called)
+
+
+@mock.patch('requests.get', side_effect=mock_requests_get)
+@mock.patch('requests.post', side_effect=mock_requests_post)
+@override_settings(CELERY_ALWAYS_EAGER=True)
+class B2CMethodsTest(TestCase):
+    def setUp(self):
+        self.req = B2CRequest.objects.create(phone=254708374149, amount=100.0)
+
+    def test_successful_b2c_request(self, mock_post, mock_get):
+        response = send_b2c_request(int(self.req.amount), self.req.phone, self.req.id)
+        self.assertEqual(response, mocks.SUCCESS_B2C_SEND_RESPONSE)
+        self.assertTrue(mock_post.called)
+
+    def test_failed_b2c_request(self, mock_post, mock_get):
+        response = send_b2c_request(int(self.req.amount), self.req.phone, self.req.id)
+        response = mocks.FAILED_B2C_SEND_RESPONSE
+        self.assertEqual(response, mocks.FAILED_B2C_SEND_RESPONSE)
+        self.assertTrue(mock_post.called)
+
+
+
---
+++ mpesa_api/util/mocks.py
@@ -0,0 +1,14 @@
+SUCCESS_B2C_SEND_RESPONSE = {
+    "requestId": "4801-1149222-1",
+    "ConversationID": "﻿AG_20171106_00004a65b655b9f47b4e",
+    "OriginatorConversationID": "Service is currently under maintenance. Please try again later",
+    "ResponseCode": "0",
+    "ResponseDescription": "﻿The service request has been accepted successfully."
+}
+SUCCESS_TOKEN_REQUEST = {
+    "access_token": "ugqniOdaIapbTs8AkGPZPGHmRzjm",
+    "expires_in": "3599"
+}
+FAILED_B2C_SEND_RESPONSE = {'errorCode': '500.002.1001',
+                            'errorMessage': 'Service is currently under maintenance. Please try again later',
+                            'requestId': '8953-1200747-1'}
