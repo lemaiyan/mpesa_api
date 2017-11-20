18229af3a81a5f1a84a417ce8811b2dbb428a0c8
lemaiyan <george.kamunya@gmail.com>
2017-11-20 19:38:07 +0300

    Set up pypi for the library

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='mpesa-api',
     version='0.1',
     packages=find_packages(),
-    description='Mpesa B2C, C2B, Online Checkout api for Django',
+    description="Mpesa B2C, C2B, STKPush Django library based on the new Api's https://developer.safaricom.co.ke",
     author='Jijo Lemaiyan',
     author_email='george@lemaiyan.xyz',
     url='https://github.com/lemaiyan/mpesa_api',
