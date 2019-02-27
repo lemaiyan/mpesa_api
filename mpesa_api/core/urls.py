36756833950ddc599a9311962828f021526bb6e5
Lemaiyan <george.kamunya@gmail.com>
2019-02-27 21:46:42 +0300

    Fixed the malformed url

--- mpesa_api/core/urls.py
+++ mpesa_api/core/urls.py
@@ -6,7 +6,7 @@ from mpesa_api.core.views import B2cResult, B2cTimeOut, C2bConfirmation, C2bVali
 app_name = 'mpesa'
 
 urlpatterns = [
-    path('b2c/timeout$', B2cTimeOut.as_view(), name='b2c_timeout'),
+    path('b2c/timeout', B2cTimeOut.as_view(), name='b2c_timeout'),
     path('b2c/result', B2cResult.as_view(), name='b2c_result'),
     path('c2b/confirmation', C2bConfirmation.as_view(), name='c2b_confirmation'),
     path('c2b/validate', C2bValidation.as_view(), name='c2b_validation'),
