eb7dbea9b3c9c0c7219954520e17ad07117227f6
Lemaiyan <george.kamunya@gmail.com>
2020-07-12 13:38:41 +0300

    Add decouple For pulling secrets from the .env file

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,41 +1,5 @@
 from .base import *
-import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DB_NAME'],
-        'USER': os.environ['DB_USER'],
-        'PASSWORD': os.environ['DB_PASSWORD'],
-        'HOST': os.environ['DB_HOST'],
-        'PORT': os.environ['DB_PORT'],  # Set to empty string for default.
-    }
-}
-
-SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
-
-# Celery
-CELERY_BROKER_URL = 'amqp://guest:guest@rabbitmq:5672//'
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-        'logfile': {
-            'level':'DEBUG',
-            'class':'logging.FileHandler',
-            'filename': BASE_DIR + "/../logfile",
-        },
-    },
-    'root': {
-        'level': 'INFO',
-        'handlers': ['console', 'logfile']
-    },
-}
